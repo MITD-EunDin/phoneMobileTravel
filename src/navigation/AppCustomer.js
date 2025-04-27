d70a07c5e1645411ed16fba6fa59b82f74132409
nguyenthitrinh1 <nttrinh321@gmail.com>
2025-04-27 20:55:23 +0700

    Thay doi luu thong tin tai AsyncStorage

--- src/navigation/AppCustomer.js
+++ src/navigation/AppCustomer.js
@@ -7,8 +7,75 @@ import OrderScreen from '../screens/user/order/OrderScreen';
 import BottomNav from '../components/Bottom/BottomNavi';
 import CommonHeader from '../components/CommonHeader'; // Import CommonHeader
 import TopBar from "../components/Top/TopBar";
+import { createStackNavigator } from '@react-navigation/stack';
+import TourDetails from '../screens/user/toudetail/TourDetail';
 
 const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+const HomeStack = () => (
+  <Stack.Navigator>
+    <Stack.Screen
+      name="Home"
+      component={HomeScreen}
+      options={{ header: (props) => <TopBar />, headerShown: true }}
+    />
+    <Stack.Screen
+      name="TourDetails"
+      component={TourDetails}
+      options={{ header: (props) => <CommonHeader {...props} title="Chi Tiết Tour" />, headerShown: true }}
+    />
+    
+  </Stack.Navigator>
+);
+
+const TourStack = () => (
+  <Stack.Navigator>
+    <Stack.Screen
+      name="Tour"
+      component={TourScreen}
+      options={{ header: (props) => <CommonHeader {...props} title="Tour" />, headerShown: true }}
+    />
+    <Stack.Screen
+      name="TourDetails"
+      component={TourDetails}
+      options={{ header: (props) => <CommonHeader {...props} title="Chi Tiết Tour" />, headerShown: true }}
+    />
+   
+  </Stack.Navigator>
+);
+
+const OrderStack = () => (
+  <Stack.Navigator>
+    <Stack.Screen
+      name="Order"
+      component={OrderScreen}
+      options={{ header: (props) => <CommonHeader {...props} title="Đơn hàng" />, headerShown: true }}
+    />
+    <Stack.Screen
+      name="TourDetails"
+      component={TourDetails}
+      options={{ header: (props) => <CommonHeader {...props} title="Chi Tiết Tour" />, headerShown: true }}
+    />
+    
+  </Stack.Navigator>
+);
+
+// Stack Navigator cho tab Tài khoản
+const AccountStack = () => (
+  <Stack.Navigator>
+    <Stack.Screen
+      name="Account"
+      component={AccountScreen}
+      options={{ header: (props) => <CommonHeader {...props} title="Tài khoản" />, headerShown: true }}
+    />
+    <Stack.Screen
+      name="TourDetails"
+      component={TourDetails}
+      options={{ header: (props) => <CommonHeader {...props} title="Chi Tiết Tour" />, headerShown: true }}
+    />
+   
+  </Stack.Navigator>
+);
 
 const CustomerNavigator = ({ navigation, route }) => {
   return (
@@ -16,41 +83,11 @@ const CustomerNavigator = ({ navigation, route }) => {
       tabBar={(props) => <BottomNav {...props} />}
       screenOptions={{ headerShown: false }}
     >
-      <Tab.Screen
-        name="homes"
-        component={HomeScreen}
-        options={{ 
-          header: (props) => <TopBar/>, 
-          headerShown: true 
-        }}
-      />
-      <Tab.Screen
-        name="tour"
-        component={TourScreen}
-        options={{ 
-          header: (props) => <CommonHeader {...props} title="Tour" />, 
-          headerShown: true 
-        }}
-      />
-      
-      <Tab.Screen
-        name="order"
-        component={OrderScreen}
-        options={{ 
-          header: (props) => <CommonHeader {...props} title="Đơn hàng" />, 
-          headerShown: true 
-        }}
-      />
-      <Tab.Screen
-        name="account"
-        component={AccountScreen}
-        options={{ 
-          header: (props) => <CommonHeader {...props} title="Tài khoản" />, 
-          headerShown: true 
-        }}
-      />
+      <Tab.Screen name="homes" component={HomeStack} />
+      <Tab.Screen name="tour" component={TourStack} />
+      <Tab.Screen name="order" component={OrderStack} />
+      <Tab.Screen name="account" component={AccountStack} />
     </Tab.Navigator>
   );
 };
-
 export default CustomerNavigator;
